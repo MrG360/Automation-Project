c273797bb6bfa0a51b469063d200af2bc6230928
Gaurav Kumar <gaurav.kumar@esq.com>
2023-01-19 23:32:07 +0530

    Added get text method

--- Automation Java/src/main/java/org/automation/Main.java
+++ Automation Java/src/main/java/org/automation/Main.java
@@ -1,17 +1,25 @@
 package org.automation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.wikipedia.org/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.findElement(By.id("js-link-box-en")).click();
+
+        out.println(driver.findElement(By.id("Welcome_to_Wikipedia")).getText());
         driver.quit();
     }
 }
